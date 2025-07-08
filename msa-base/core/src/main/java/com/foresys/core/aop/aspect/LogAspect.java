655c448bcfe69d19537328fbc7a74bfe992f9a21
Hyunni <143974589+hyunsDevelopment@users.noreply.github.com>
2025-07-08 13:39:27 +0900

    Add files via upload

--- msa-base/core/src/main/java/com/foresys/core/aop/aspect/LogAspect.java
+++ msa-base/core/src/main/java/com/foresys/core/aop/aspect/LogAspect.java
@@ -23,24 +23,24 @@ public class LogAspect {
 
 	@Around("execution(* com.foresys..*Controller.*(..)) && args(params,..)")
 	public Object logBefore(ProceedingJoinPoint joinpoint, Object params) throws Throwable {
-		try {
-			log.info("==== LogAspect.logBefore ReqParam : {}", ConvertUtil.ObjToJsonstring(params));
-		}catch (Exception e) {
-			log.info("==== LogAspect.logBefore ReqParam : Log cannot be printed.");
-		}
+		printLog(params, true);
 
 		Object proceed = joinpoint.proceed();
 
-		try {
-			Object res;
-			if (proceed instanceof Map) res = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(proceed);
-			else res = proceed;
-			log.info("==== LogAspect.logBefore ResParam : {}", ConvertUtil.ObjToJsonstring(res));
-		} catch (Exception e) {
-			log.info("==== LogAspect.logBefore ReqParam : Log cannot be printed.");
-		}
+		printLog(proceed, false);
 
 		return proceed;
+	}
+
+	private static void printLog(Object params, boolean isReq) {
+		try {
+			Object res;
+			if (params instanceof Map) res = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(params);
+			else res = params;
+			log.info("==== LogAspect.logBefore {}Param : {}", isReq ? "Req" : "Res", ConvertUtil.ObjToJsonstring(res));
+		}catch (Exception e) {
+			log.info("==== LogAspect.logBefore {}Param : Log cannot be printed.", isReq ? "Req" : "Res");
+		}
 	}
 
 }
